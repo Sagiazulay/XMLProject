982efed905bfbf51df03021a3bd4df16ef560ed3
Sagiazulay <58825185+Sagiazulay@users.noreply.github.com>
2020-08-23 19:17:40 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLProject
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        private int codan;
+
+        public int Codan
+        {
+            get { return codan; }
+            set { codan = value; }
+        }
+        protected int numberOfSeats = 4;
+
+        public Car()
+        {
+                
+        }
+        //public Car(FileName fileName)
+       // {
+
+        //}
+
+        public Car(string model, string brand, int year, string color, int codan, int numberOfSeats)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+            Codan = codan;
+            this.numberOfSeats = numberOfSeats;
+        }
+        public int GetNumberOfSeats()
+        {
+            return numberOfSeats;
+        }
+        protected void ChangeNumberOfSeats(int newNumber)
+        {
+            if(newNumber > 0)
+            {
+                newNumber = numberOfSeats;
+            }
+        }
+        public static void SerializeACar(string filename, Car car)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car));
+
+            using (Stream file = new FileStream(filename, FileMode.Create))
+            {
+                xmlSerializer.Serialize(file, car);
+            }
+        }
+        public static void SerializeACarArray(string filename, Car [] cars)
+        {
+            XmlSerializer xmlSerializerArray = new XmlSerializer(typeof(Car[]));
+
+            using (Stream file = new FileStream(filename, FileMode.Create))
+            {
+                xmlSerializerArray.Serialize(file, cars);
+            }
+        }
+        public static Car DeserializeACar(string filename)
+        {
+            Car result = null;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car));
+
+            using (Stream file = new FileStream(filename, FileMode.Open))
+            {
+                result = xmlSerializer.Deserialize(file) as Car;
+            }
+            return result;
+        }
+        public static Car [] DeserializeACarArray(string filename)
+        {
+            Car[] cars = null;
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Car[]));
+
+            using (Stream file = new FileStream(filename, FileMode.Open))
+            {
+                cars = xmlSerializer.Deserialize(file) as Car[];
+            }
+            return cars;
+        }
+        public override string ToString()
+        {
+            return $"Car Modle : {this.Model} Brand : {this.Brand} Year : {this.Year} Color : {this.Color}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Mazda = new Car("Mazda", "3", 2008, "Yellow", 1234, 4);
+            Car.SerializeACar(@"c:\temp\xmlfile.xml", Mazda);
+
+            Car Fiat;
+            Fiat = Car.DeserializeACar(@"c:\temp\xmlfile.xml") as Car;
+            Console.WriteLine(Fiat.ToString());
+
+            Car[] cars = new Car[]
+            {
+                new Car("Mercedes", "GLA", 2019, "White", 4545, 2),
+                new Car("Chevrolet", "Optra", 2008, "Grey", 1388, 5),
+                new Car("Fiat", "500", 2015, "Red", 008, 4)
+            };
+            Car.SerializeACarArray(@"c:\temp\xmlarrayfile.xml", cars);
+
+            Car[] NewArray;
+            NewArray = Car.DeserializeACarArray(@"c:\temp\xmlarrayfile.xml");
+            foreach (var item in NewArray)
+            {
+                Console.WriteLine(item);
+            }
+            
+
+        }
+    }
+}
